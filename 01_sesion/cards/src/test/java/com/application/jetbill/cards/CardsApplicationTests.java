d482081173098165646f10204ccbf15dd9d1d2f7
jetbill <william.renteria.m@gmail.com>
2024-11-28 20:38:12 -0500

    cards microservice completed

---
+++ 01_sesion/cards/src/test/java/com/application/jetbill/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.jetbill.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
